edb12f953802762fe66cd2a4de90c7e0b41bd9b9
Jens Andreasson <jensandreasson77@gmail.com>
2023-11-29 12:20:54 +0100

    💄 Refactor CSS styles and update layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from '@/app/components/header'
+import "modern-normalize/modern-normalize.css"
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -14,9 +12,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html >
   )
 }
